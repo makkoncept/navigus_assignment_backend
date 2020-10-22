80c379412209c56694b2b953a7793ae1a42f6093
Mayank Nader <mayank.nader11@gmail.com>
2020-10-22 20:18:35 +0530

    create a /login route to validate if user exists in database

--- server.py
+++ server.py
@@ -212,11 +212,41 @@ class Course(Resource):
         courses = CourseModel.query.all()
         return {'results': marshal(courses, course_fields)}, 204
 
+class Login(Resource):
+    # method_decorators = {
+    #     "get": [auth.login_required],
+    #     "put": [permission_required("admin"), auth.login_required],
+    #     "delete": [permission_required("admin"), auth.login_required],
+    # }
+
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument("username", type=str, location="json")
+        self.reqparse.add_argument("password", type=str, location="json")
+        self.reqparse.add_argument("role", type=str, location="json")
+        super(Login, self).__init__()
+
+    def post(self):
+        args = self.reqparse.parse_args()
+        print(args)
+        user = ''
+        if args['role'] == 'student':
+            user = StudentModel.query.filter_by(username=args['username']).first()
+        elif args['role'] == 'teacher':
+            user = TeacherModel.query.filter_by(username=args['username']).first()
+        else:
+            abort(422) # not a valid role
+
+        if not user or user.password != args['password']:
+            abort(401) # unauthorized
+
+        return {'results': args}
 
 api.add_resource(StudentList, "/students/")
 api.add_resource(TeacherList, "/teachers/")
 api.add_resource(CourseList, "/courses/")
 api.add_resource(Course, "/courses/<int:course_id>")
+api.add_resource(Login, "/login/")
 
 if __name__ == "__main__":
     app.run(debug=True)
