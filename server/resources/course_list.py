0c08b9ab0c331c61a7e4f639dce365d020fd148a
Mayank Nader <mayank.nader11@gmail.com>
2020-10-23 03:35:53 +0530

    return 409 if adding course with already present code

--- server/resources/course_list.py
+++ server/resources/course_list.py
@@ -1,8 +1,9 @@
-from flask_restful import Resource, reqparse, marshal
+from flask_restful import Resource, reqparse, marshal, abort
 
 from server import db
 from server.models import CourseModel
 from server.common.utils import course_fields
+
 
 class CourseList(Resource):
     def __init__(self):
@@ -18,19 +19,24 @@ class CourseList(Resource):
 
     def get(self):
         courses = CourseModel.query.all()
-        return {'results': marshal(courses, course_fields)}
+        return {"results": marshal(courses, course_fields)}
 
     def post(self):
+        print("inside post")
         args = self.reqparse.parse_args()
         print(args)
 
-        # TODO: handle if course code already exists
+        if (
+            CourseModel.query.filter_by(course_code=args["course_code"]).first()
+            is not None
+        ):
+            abort(409)  # conflict
 
-        course = CourseModel(course_code=args["course_code"].lower(), name=args["name"].lower())
+        course = CourseModel(
+            course_code=args["course_code"].lower(), name=args["name"].lower()
+        )
 
         db.session.add(course)
         db.session.commit()
 
-        return {'results': marshal(course, course_fields)}, 201
-
-
+        return {"results": marshal(course, course_fields)}, 201
