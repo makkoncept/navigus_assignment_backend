515ade48264ab0a4e933da06a24e7339266c37d4
Mayank Nader <mayank.nader11@gmail.com>
2020-10-23 03:30:40 +0530

    create question and options db models

--- server/models.py
+++ server/models.py
@@ -1,5 +1,6 @@
 from server import db
 
+
 class StudentModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(32), nullable=False, index=True)
@@ -22,8 +23,33 @@ class TeacherModel(db.Model):
 
 class CourseModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    course_code = db.Column(db.String(10))
-    name = db.Column(db.String(100))
+    course_code = db.Column(db.String(10), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    passing_marks = db.Column(db.Integer, nullable=False)
+    questions = db.relationship("QuestionModel", backref="course", lazy=True)
 
     def __repr__(self):
-        return f"{self.id} | {self.course_code} | {self.name}"
+        return f"{self.id} | {self.course_code} | {self.name} | {self.questions}"
+
+
+class QuestionModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.Text(), nullable=False)
+    marks = db.Column(db.Integer, nullable=False)
+    course_id = db.Column(db.Integer, db.ForeignKey("course_model.id"), nullable=False)
+    options = db.relationship("OptionModel", backref="question", lazy=True)
+
+    def __repr__(self):
+        return f"{self.id} | {self.text} | {self.options}"
+
+
+class OptionModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(250), nullable=False)
+    isTrue = db.Column(db.Boolean, nullable=False)
+    question_id = db.Column(
+        db.Integer, db.ForeignKey("question_model.id"), nullable=False
+    )
+
+    def __repr__(self):
+        return f"{self.id} | {self.text} | {self.question_id}"
