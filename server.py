403c606558817f705e9cdc897e6a319c67e6cd9a
Mayank Nader <mayank.nader11@gmail.com>
2020-10-22 12:42:50 +0530

    enable CORS using Flask-CORS

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
+from flask_cors import CORS
 from flask_restful import Api, Resource, reqparse, abort, fields, marshal
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 api = Api(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 db = SQLAlchemy(app)
