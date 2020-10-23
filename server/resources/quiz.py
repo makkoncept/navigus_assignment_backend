9a5395b887b296b0b6e3c80a51f5cb5396fc5ae2
Mayank Nader <mayank.nader11@gmail.com>
2020-10-23 16:36:29 +0530

    update the post method of quiz to successfully receive the options

--- server/resources/quiz.py
+++ server/resources/quiz.py
@@ -8,18 +8,8 @@ from server.common.utils import question_fields
 class Quiz(Resource):
     def __init__(self):
         self.reqparse = reqparse.RequestParser()
-        """ Due to the limitations of request parser, partial validation will be done here. It 
-        validates that 'text' key which contains string exist and an 'options' key exists which
-        should be a array of dictionary. The sanity check on the dictionary will be done while
-        parsing it."""
         self.reqparse.add_argument(
-            "text",
-            type=str,
-            required=True,
-            location="json",
-        )
-        self.reqparse.add_argument(
-            "options", type=dict, required=True, location="json", action="append"
+            "options", type=bool, required=True, location="json", action="append"
         )
         super(Quiz, self).__init__()
 
@@ -37,3 +27,29 @@ class Quiz(Resource):
         # print(questions)
         # return {"results": marshal(courses, course_fields)}
         return {"results": marshal(target_question, question_fields)}
+
+    def post(self, course_id, question_no):
+        args = self.reqparse.parse_args()
+        print(args, course_id, question_no)
+
+        course = CourseModel.query.filter_by(id=course_id).first()
+        if course is None:
+            abort(404, message=f"course with course id = {course_id} does not exist")
+
+        questions = course.questions
+        if len(questions) < question_no:
+            return {"results": "no question left"}
+
+        target_question = questions[question_no - 1]
+        options = target_question.options
+        print(options)
+
+        answers = []
+        for option in options:
+            if option.is_true:
+                answers.append(True)
+            else:
+                answers.append(False)
+
+        print(answers)
+        return {"results": "received"}
